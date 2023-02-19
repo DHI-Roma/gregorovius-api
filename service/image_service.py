48b76055217de110e0eee36b780236dbff7c20d1
Oliver Pohl <oliverpohl@wildegans-solutions.de>
2023-02-19 16:57:43 +0100

    FG-03-14: Load files alphabetically

--- service/image_service.py
+++ service/image_service.py
@@ -4,7 +4,8 @@ from pathlib import Path
 def generate_image_map():
     img_dir = Path('./img/webp')
     img_map = {}
-    for img in img_dir.iterdir():
+    position_map = {}
+    for img in sorted(img_dir.iterdir()):
         parts = img.stem.split("_")
 
         identifier_parts_delimiter = -1
@@ -13,20 +14,18 @@ def generate_image_map():
             identifier_parts_delimiter = -2
 
         letter_id = "_".join(parts[:identifier_parts_delimiter])
-        position_label = parts[-1]
-
-        if position_label.isnumeric():
-            position = int(position_label)
-        elif "r" in position_label:
-            position = ((int(re.sub(r'[a-z]', "", position_label)) - 1) * 2) + 1
-        elif "v" in position_label:
-            position = int(re.sub(r'[a-z]', "", position_label)) * 2
+        label = parts[-1]
 
         if letter_id not in img_map:
             img_map[letter_id] = {}
-        img_map[letter_id][position] = {
+            position_map[letter_id] = 0
+
+        img_map[letter_id][position_map[letter_id]] = {
             'name': img.name,
-            'label': position_label
+            'label': label
         }
 
+        position_map[letter_id] += 1
+
     return img_map
+
