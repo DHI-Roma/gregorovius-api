04396749f8c4ea26dd2934ddd2a8b1d34ef16d0f
Oliver Pohl <oliverpohl@wildegans-solutions.de>
2023-05-15 16:21:07 +0200

    FG-03-06: Extend full letter index

--- service/letter_index_service.py
+++ service/letter_index_service.py
@@ -7,6 +7,7 @@ def parse_gesamtdatenbank():
     unique_recipients = []
     unique_sender_places = []
     unique_recipient_places = []
+    unique_years = []
     results = []
     date_earliest = ''
     date_latest = ''
@@ -15,6 +16,9 @@ def parse_gesamtdatenbank():
         csvreader = csv.DictReader(csvfile, delimiter=';')
 
         for entry in csvreader:
+            if not len(entry['sender-1']) and not len(entry['recipient-1']):
+                continue
+
             senders = [entry['sender-1']]
 
             if entry['sender-1'] not in unique_senders and len(entry['sender-1']) :
@@ -44,6 +48,7 @@ def parse_gesamtdatenbank():
                 unique_recipient_places.append(entry['recipient-placeName'])
 
 
+            relevant_years = []
             date_earliest_candidates = []
             if entry['date-when'].startswith('1'):
                 date_earliest_candidates.append(entry['date-when'])
@@ -53,6 +58,14 @@ def parse_gesamtdatenbank():
 
             if entry['date-notBefore'].startswith('1'):
                 date_earliest_candidates.append(entry['date-notBefore'])
+
+            for date in date_earliest_candidates:
+                year = date.split('-')[0]
+                if year not in unique_years:
+                    unique_years.append(year)
+
+                if year not in relevant_years:
+                    relevant_years.append(year)
 
 
             if len(date_earliest_candidates):
@@ -69,11 +82,19 @@ def parse_gesamtdatenbank():
             if entry['date-when'].startswith('1'):
                 date_latest_candidates.append(entry['date-when'])
 
+            if entry['date-when'].startswith('0'):
+                relevant_years.append(entry['date-when'].split('-')[0])
+
             if entry['date-to'].startswith('1'):
                 date_latest_candidates.append(entry['date-to'])
 
             if entry['date-notAfter'].startswith('1'):
                 date_latest_candidates.append(entry['date-notAfter'])
+
+            for date in date_latest_candidates:
+                year = date.split('-')[0]
+                if year not in unique_years:
+                    unique_years.append(year)
 
             if len(date_latest_candidates):
                 date_latest_candidate = min(date_latest_candidates)
@@ -81,12 +102,28 @@ def parse_gesamtdatenbank():
                 if date_latest_candidate and not date_latest:
                     date_latest = date_latest_candidate
 
+                if year not in relevant_years:
+                    relevant_years.append(year)
+
                 date_latest = max([date_latest, date_latest_candidate])
+
+            date_index_hierarchy = ['date-when', 'date-to', 'date-notAfter', 'date-notBefore']
+            date_index = None
+            for hierarchy_step in date_index_hierarchy:
+                if not date_index:
+                    date_index = entry[hierarchy_step].strip() if (len(entry[hierarchy_step].strip())) else None
+
+            if not date_index:
+                date_index = '0000-00-00'
+
+            ## Ensure that it will be sorted at end of year
+            date_index = date_index.replace('-00', '-99').replace('0000', '9999')
 
             result = {
                 'index': index,
                 'xml_id': entry['xml-id'],
                 'lfdnr': entry['lfdnr'],
+                'date_index': date_index,
                 'status': entry['auswahl'],
                 'senders': senders,
                 'recipients': recipients,
@@ -98,6 +135,7 @@ def parse_gesamtdatenbank():
                 'date_not_before': entry['date-notBefore'] if len(entry['date-notBefore']) > 0 else None,
                 'date_not_after': entry['date-notAfter'] if len(entry['date-notAfter']) > 0 else None,
                 'date_when':entry['date-when'].strip() if len(entry['date-when'].strip()) > 0 else None,
+                'relevant_years': relevant_years,
                 'incipit': entry['incipit'],
                 'scope': entry['Umfang'],
                 'reference': entry['Handschrift- oder Abschriftennachweis'],
@@ -107,17 +145,23 @@ def parse_gesamtdatenbank():
             results.append(result)
             index += 1
 
+
         unique_senders.sort()
         unique_recipients.sort()
         unique_sender_places.sort()
         unique_recipient_places.sort()
+        unique_years.sort()
+        unique_years.append("0000")
+
+        letters = sorted(results, key=lambda d: d['date_index'])
 
     return {
-        'letters': results,
+        'letters': letters,
         'unique_senders': unique_senders,
         'unique_recipients': unique_recipients,
         'unique_sender_places': unique_sender_places,
         'unique_recipient_places': unique_recipient_places,
+        'unique_years': unique_years,
         'aufbewahrungsorte_short': aufbewahrungsorte,
         'date_earliest': date_earliest,
         'date_latest': date_latest
